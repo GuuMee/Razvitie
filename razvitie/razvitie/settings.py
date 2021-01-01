4de41dc5695e9d911eb638ad10758c35c13a439a
guumee <guzelya93@gmail.com>
2021-01-01 16:52:30 +0300

    Some changes, correcting the format of pics and setting allow hosts

--- razvitie/razvitie/settings.py
+++ razvitie/razvitie/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = 't^k_$oxewl!i+c!c3+7lw4p8*9h)3kbb-!1kih42a1@#-cxn=l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.razvitiye23.ru']
 
-
+DEFAULT_CHARSET = 'utf-8'
 # Application definition
 
 INSTALLED_APPS = [
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'razvitie.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'razvitie',
+        'USER': 'postgres',
+        'PASSWORD': '2703',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
